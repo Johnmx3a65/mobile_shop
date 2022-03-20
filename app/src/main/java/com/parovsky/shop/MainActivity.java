e25cd00e846911f569a5aa13ad5d3273e84830f7
fastalb <321gidchannel123@gmail.com>
2022-03-20 19:42:04 +0200

    pososal hui

--- app/src/main/java/com/parovsky/shop/MainActivity.java
+++ app/src/main/java/com/parovsky/shop/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("Hello World");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
