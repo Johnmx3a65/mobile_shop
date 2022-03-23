820a5d24dfe32adf1130c381b7afafe50074c2a8
Ivan Stoianov <ivan.stoianov@sap.com>
2022-03-23 22:05:36 +0200

    finished greeting page

--- app/src/main/java/com/parovsky/shop/MainActivity.java
+++ app/src/main/java/com/parovsky/shop/MainActivity.java
@@ -2,12 +2,41 @@ package com.parovsky.shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button openLogInBtn = findViewById(R.id.logInButtonGreeting);
+        openLogInBtn.setOnClickListener(this::openLoginPage);
+
+        TextView openSignInTxt = findViewById(R.id.signInTextGreeting);
+        openSignInTxt.setOnClickListener(this::openSignIn);
+    }
+
+    /**
+     * Методът се задейства, когато Login бутона е натиснат
+     *
+     * @param view
+     */
+    private void openLoginPage(View view) {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    /**
+     * Методът се задейства, когато Sign in текста е натиснат
+     *
+     * @param view
+     */
+    private void openSignIn(View view) {
+        startActivity(new Intent(MainActivity.this, SignInActivity.class));
     }
 }
