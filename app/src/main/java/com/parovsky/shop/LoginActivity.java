874f7c1f5292db99450b35af82c01c4236bb85fe
I553730 <ivanstojanoff2015@gmail.com>
2022-04-04 10:56:36 +0300

    backend logic for greeting page is completed

--- app/src/main/java/com/parovsky/shop/LoginActivity.java
+++ app/src/main/java/com/parovsky/shop/LoginActivity.java
@@ -2,17 +2,13 @@ package com.parovsky.shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
