efa0d9ae36b77afac636bf942d93b615525ccc47
fastalb <321gidchannel123@gmail.com>
2022-04-11 18:29:01 +0300

    name register page, create new password page

---
+++ app/src/main/java/com/parovsky/shop/NewPassword.java
@@ -0,0 +1,13 @@
+package com.parovsky.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_password);
+    }
+}
