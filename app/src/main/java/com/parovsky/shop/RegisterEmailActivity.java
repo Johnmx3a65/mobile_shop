4c9ae21286256b83d98df10941f3a769465becd1
fastalb <321gidchannel123@gmail.com>
2022-04-15 14:31:07 +0300

    all register pages

---
+++ app/src/main/java/com/parovsky/shop/RegisterEmailActivity.java
@@ -0,0 +1,14 @@
+package com.parovsky.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_email);
+    }
+}
